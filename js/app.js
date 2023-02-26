1b5f1275537c00a2217ff13cb427b38f38a58233
Mohsen Shirazi <mohsensh4792@gmail.com>
2023-02-26 03:22:37 -0500

    Added the photo alts, stylings to the search function

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@
 
 const imagesGridEl = document.querySelector(".gallery"); // The element that targets the div containing the images  (grid)
 
-const photoCaptions = ["I love hay bales. Took this snap on a drive through the countryside past some straw fields.", "The lake was so calm today. We had a great view of the snow on the mountains from here.", "I hiked to the top of the mountain and got this picture of the canyon and trees below.", "It was amazing to see an iceberg up close, it was so cold but didn’t snow today.", "The red cliffs were beautiful. It was really hot in the desert but we did a lot of walking through the canyons.", "Fall is coming, I love when the leaves on the trees start to change color.", "I drove past this plantation yesterday, everything is so green!", "My summer vacation to the Oregon Coast. I love the sandy dunes!", "We enjoyed a quiet stroll down this countryside lane.", "Sunset at the coast! The sky turned a lovely shade of orange.", "I did a tour of a cave today and the view of the landscape below was breathtaking.", "I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in."]
+const photoCaptions = ["I love hay bales. Took this snap on a drive through the countryside past some straw fields.", "The lake was so calm today. We had a great view of the snow on the mountains from here.", "I hiked to the top of the mountain and got this picture of the canyon and trees below.", "It was amazing to see an iceberg up close, it was so cold but didn’t snow today.", "The red cliffs were beautiful. It was really hot in the desert but we did a lot of walking through the canyons.", "Fall is coming, I love when the leaves on the trees start to change color.", "I drove past this plantation yesterday, everything is so green!", "My summer vacation to the Oregon Coast. I love the sandy dunes!", "We enjoyed a quiet stroll down this countryside lane.", "Sunset at the coast! The sky turned a lovely shade of orange.", "I did a tour of a cave today and the view of the landscape below was breathtaking.", "I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in."];
+
+const photoAlts = ["a country farm", "a mountain near the sea", "cloudy weather near the Irish coast", "mountain with iceberg", "a desert", "a path to an abandoned gate", "a lively green area", "a beach hill", "an overcoast tunnel", "an ocean hill", "mountain with a smoke valley", "lavender pasture before the snowcapped moutaint"]
 
 let images = '';
 
@@ -10,7 +12,7 @@ let images = '';
 function addPhotosToHTML(){
     for(let i = 0; i < 12; i++){
         images += `
-        <a href="photos/0${i + 1}.jpg" data-caption="${photoCaptions[i]}" class="img-link"><img src="photos/thumbnails/${i + 1}.jpg" class="pic-${i + 1}" alt="${photoCaptions[i]}"></a>
+        <a href="photos/0${i + 1}.jpg" data-caption="${photoCaptions[i]}" class="img-link"><img src="photos/thumbnails/${i + 1}.jpg" class="pic-${i + 1} pic" alt="${photoAlts[i]}"></a>
         `
     }
 }
@@ -29,3 +31,10 @@ window.addEventListener('load', function() {
 
 
 
+// The Search Button
+
+let searchButton = document.querySelector('.search-button');
+
+searchButton.addEventListener('click', () => {
+    searchButton.parentElement.classList.toggle('open');
+})
--- js/searchFunction.js
+++ js/searchFunction.js
@@ -4,13 +4,17 @@ let searchInputEl = document.getElementById('search-input');
 searchInputEl.addEventListener('keyup', e => {
     let searchInputValue = e.target.value.toLowerCase();
     let imageLinkElements = document.querySelectorAll('.img-link');
+    let images = document.querySelectorAll('.pic');
     console.log(imageLinkElements);
     imageLinkElements.forEach(imageLinkElement => {
-        if (imageLinkElement.getAttribute('data-caption').toLowerCase().includes(searchInputValue)){
-            imageLinkElement.style.display = 'block'
-        } else {
-            imageLinkElement.style.display = 'none'
-        }
+        images.forEach(image => {
+            if (imageLinkElement.getAttribute('data-caption').toLowerCase().includes(searchInputValue) || image.getAttribute('alt').includes(searchInputValue)){
+                imageLinkElement.style.display = 'block';
+            } else {
+                imageLinkElement.style.display = 'none';
+            }
+        }) 
     })
+
 })
 
